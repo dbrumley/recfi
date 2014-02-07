4baf1fc4f1fda5de0642b99f5bf346d49aa93fc7
jb295 <jb295@buffalo.edu>
2014-02-07 01:03:22 -0500

    working instruction pass

--- llvm/lib/Transforms/Test/Test.cpp
+++ llvm/lib/Transforms/Test/Test.cpp
@@ -14,6 +14,8 @@
 #define DEBUG_TYPE "test_pass"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/BasicBlock.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 using namespace llvm;
@@ -21,18 +23,35 @@ using namespace llvm;
 STATISTIC(TestCounter, "Counts number of functions greeted");
 
 namespace {
-  // Test - The first implementation, without getAnalysisUsage.
-  struct Test : public FunctionPass {
-    static char ID; // Pass identification, replacement for typeid
-    Test() : FunctionPass(ID) {}
+    // Test - The first implementation, without getAnalysisUsage.
+    struct Test : public FunctionPass {
+        static char ID; // Pass identification, replacement for typeid
+        Test() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      ++TestCounter;
-      errs() << "Test: ";
-      errs().write_escaped(F.getName()) << '\n';
-      return false;
-    }
-  };
+        virtual bool runOnFunction(Function &F) {
+            ++TestCounter;
+            errs() << "Test: ";
+            errs().write_escaped(F.getName()) << '\n';
+            for (Function::iterator I = F.begin(), E = F.end(); I != E; ++I)
+            {
+                runOnBasicBlock(*I);
+            }
+           return false;
+        }
+        
+        virtual bool runOnBasicBlock(BasicBlock &B) {
+            for (BasicBlock::iterator I = B.begin(), E = B.end(); I != E; ++I)
+            {
+                runOnInstruction(*I);
+            }
+           return false;
+        }
+        
+        virtual bool runOnInstruction(Instruction &I) {
+                errs().write_escaped(I.getOpcodeName()) << '\n';
+                return false;
+        }
+    };
 }
 
 char Test::ID = 0;
