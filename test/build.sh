36f73aaefce10a10c1a20f88927b6b602d0614cd
Oulin Yao <oyao@andrew.cmu.edu>
2014-02-17 18:25:56 -0500

    added my scripts to build.sh

--- test/build.sh
+++ test/build.sh
@@ -1,6 +1,11 @@
 clang -O0 -emit-llvm test.c -c -o test.bc
 clang -O0 -emit-llvm test.c -S -o test.ll
-opt -load ../build/Debug+Asserts/lib/LLVMTest.so -test < test.bc > /dev/null
+#the following is how I build:
+#llvm-gcc -O3 -emit-llvm -S test.c -o test.ll
+#llvm-as test.ll -o test.bc
+#llc -O0 -march arm test.bc -o test.s
+
+#opt -load ../build/Debug+Asserts/lib/LLVMTest.so -test < test.bc > /dev/null
 #opt -dot-callgraph < test.bc > /dev/null
 #dot -Tpng callgraph.dot  -o outpng
 #eog outpng
