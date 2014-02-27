107a62f5f94ccc5f9d4b49ac1da70fb8b340549d
Oulin Yao <oyao@andrew.cmu.edu>
2014-02-27 14:18:30 -0500

    added cfi_data to instruction

--- test/run_lynn.sh
+++ test/run_lynn.sh
@@ -2,9 +2,11 @@
 #clang -O0 -emit-llvm test.c -S -o test.ll
 
 echo "compiling..."
-llvm-gcc -O0 -emit-llvm -S $1.c -o $1.ll
-echo "assembling llvm bitcode..."
-llvm-as $1.ll -o $1.bc
+clang -O0 -emit-llvm $1.c -c -o $1.bc
+clang -O0 -emit-llvm $1.c -S -o $1.ll
+#llvm-gcc -O0 -emit-llvm -S $1.c -o $1.ll
+#echo "assembling llvm bitcode..."
+#llvm-as $1.ll -o $1.bc
 echo "running $2 pass..."
 opt -load ../build/projects/poolalloc/Release+Debug+Asserts/lib/LLVMDataStructure.dylib -load ../llvm-cfi/build/Release+Asserts/lib/LLVM$2.dylib -$2 < $1.bc > $1_opt.bc
 echo "generating target specific assembly..."
