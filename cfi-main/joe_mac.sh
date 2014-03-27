c131505d165b153a2708abccf387f2c4b6faaa14
jb295 <jb295@buffalo.edu>
2014-03-27 11:13:33 -0400

    reorganized test to be called cfi-main

--- cfi-main/joe_mac.sh
+++ cfi-main/joe_mac.sh
@@ -2,17 +2,17 @@ echo "generating llvm IR..."
 clang -O0 -emit-llvm $1.c -S -o $1.ll
 
 echo "generating llvm bitcode..."
-llvm-as $1.ll
+clang -O3 -emit-llvm $1.c -c -o $1.bc
 
 echo "running $2 pass..."
-opt -load ../../build/projects/poolalloc/Release+Debug+Asserts/lib/LLVMDataStructure.dylib -load ../../llvm-cfi/build/Release+Debug+Asserts/lib/LLVM$2.dylib -$2 < $1.bc > $1_opt.bc
+opt -load ../build/projects/poolalloc/Release+Debug+Asserts/lib/LLVMDataStructure.dylib -load ../llvm-cfi/build/Release+Debug+Asserts/lib/LLVM$2.dylib -$2 < $1.bc > $1_opt.bc
 #opt -load ../build/Release+Debug+Asserts/lib/LLVM$2.dylib -$2 < $1.bc > $1_opt.bc
 
 echo "generating transformed llvm ir"
 llvm-dis $1_opt.bc
 
 echo "generating target specific assembly..."
-llc -O0 -march arm $1_opt.bc
+llc -O3 -march arm $1_opt.bc
 
 echo "generating target specific object..."
-llc -O0 -march arm -filetype obj test_opt.bc
+llc -O3 -march arm -filetype obj $1_opt.bc
