6b6fe2947fbb82a9fbe2c8c5cc3736c99ea05671
oulin yao <oulinyao@gmail.com>
2014-04-20 14:34:01 -0400

    modified simple test case, added lynn_droptlet.sh

---
+++ cfi-main/scripts/lynn_droplet.sh
@@ -0,0 +1,31 @@
+echo "generating llvm IR..."
+clang -target armv7a-linux-gnueabi -use-gold-plugin -emit-llvm -Wl,-plugin-opt=also-emit-llvm \
+-I/usr/lib/gcc/arm-linux-gnueabi/4.6/include -I/usr/lib/gcc/arm-linux-gnueabi/4.6/include-fixed -I/usr/arm-linux-gnueabi/include -I/usr/include \
+-static -S -o $1.ll $1.c
+
+echo "generating llvm bitcode..."
+#clang -O1 -emit-llvm $1.c -c -o $1.bc
+clang -target armv7a-linux-gnueabi -use-gold-plugin -emit-llvm -Wl,-plugin-opt=also-emit-llvm \
+-I/usr/lib/gcc/arm-linux-gnueabi/4.6/include -I/usr/lib/gcc/arm-linux-gnueabi/4.6/include-fixed -I/usr/arm-linux-gnueabi/include -I/usr/include \
+-static -o $1 $1.c
+
+echo "running $2 pass..."
+opt -stats \
+-load /home/lynn/boeing-cfi/build/projects/poolalloc/Release+Debug+Asserts/lib/LLVMDataStructure.so \
+-load /home/lynn/boeing-cfi/cfi-proj/build/Release+Debug+Asserts/lib/CfiPasses.so \
+-load /home/lynn/boeing-cfi/cfi-proj/build/Release+Debug+Asserts/lib/CfiUtil.so \
+-load /home/lynn/boeing-cfi/cfi-proj/build/Release+Debug+Asserts/lib/LLVMcfi.so \
+-cfi -$2 $1.bc -o $1_opt.bc
+
+echo "generating transformed llvm ir"
+llvm-dis $1_opt.bc
+
+echo "generating target specific assembly..."
+llc -mtriple armv7a-linux-gnueabi -o $1.s $1_opt.bc
+
+echo "running python script..."
+python /home/lynn/boeing-cfi/cfi-main/PythonScripts/AsmEditor.py -f $1.s -o $1_fixed.s
+
+echo "assembling fixed assembly..."
+arm-linux-gnueabi-gcc -static $1_fixed.s -lrt -o $1
+chmod +x $1
--- cfi-main/testing/simple/simple.c
+++ cfi-main/testing/simple/simple.c
@@ -1,72 +1,40 @@
 
-int global_var = 55;
-int global_var2;
+#include <stdio.h>
+
 int (*sum_func)(int, int);
 
 int sum(int x, int y)
 {
+    printf("in sum\n");
     return x + y;
 }
 
 int other_sum(int x, int y)
 {
+    printf("in other_sum\n");
     return x + y + 1;
-}
-
-int testing(int (*f)(int, int), int a, int b)
-{
-    return f(a, b);
 }
 
 int main()
 {
+    printf("in main\n");
     int x = 15;
     int y = 25;
-    global_var2 = 6;
-/*
-    switch(x)
-    {
-      case 10:
-        y = 20;
-	break;
-      case 15:
-        y = 16;
-	break;
-      case 16:
-        y = 21;
-	break;
-      case 28:
-        y = 31;
-	break;
-      case 29:
-        y = 68;
-	break;
-      case 50:
-        y = 89;
-	break;
-      case 70:
-        y = 89;
-	break;
-      case 71:
-        y = 82;
-	break;
-      default:
-        break;
-    }
-*/
+
     if(x < 10)
     {
+        printf("setting sum_func to sum\n");
         sum_func = sum;
     }
     else
     {
+        printf("setting sum_func to other_sum\n");
         sum_func = other_sum;
     }
 
+    printf("calling sum_func\n");
     x += sum_func(x, y);
-    x += sum(global_var, global_var2);
 
-    testing(sum_func, 3, 4);
-
-    return 0;
+    printf("done!\n");
+    return x;
 }
