6086b6e83eef4e64654546a451ae04c93a2b16f6
Rohan Sehgal <rohan.sehgal.su@gmail.com>
2015-03-16 19:16:50 -0400

    staging commit for coreutil script

---
+++ scripts/copyCoreutilBins.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-corepath', help = "path to coreutils-8.22", required = True)
+parser.add_argument('-binpath', help = "path to the binaries")
+
+opts = parser.parse_args()
+
+coreutilsPath = opts.corepath
+
+binPath = ""
+
+textchars = bytearray([7,8,9,10,12,13,27]) + bytearray(range(0x20, 0x100))
+is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
+
+def is_binary(filename):
+   return is_binary_string(open(filename, 'rb').read(1024))
+
+if opts.binpath == None:
+   binPath = coreutilsPath + "/build/src/"
+else:
+   binPath = opts.binpath
+
+for name in os.listdir(binPath):
+   filePath = os.path.join(binpath, name)
+   if os.path.isfile(filePath):
+      if is_binary(filePath):
+         print filePath
+
+print is_binary('/bin/ls')
+ 
